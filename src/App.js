9dd114e66ae2e42072c5d53be407a4092040d4ca
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2020-05-18 16:17:51 +0530

    Initial repo with Complete Frontend On React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React,{Component} from 'react';
-import UserFrom from './components/UserForm';
-
-import logo from './logo.svg';
 import './App.css';
 import UserForm from './components/UserForm';
 
 export default class App extends Component{
+  componentDidMount=()=>{
+    document.title='User Multistep Form';
+  }
   render=()=>{
     return (<div className='App'>
       <UserForm/>
